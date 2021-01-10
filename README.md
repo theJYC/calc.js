2b5198925fe202ca3305f2a1447c9de7e4135d36
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-01-10 12:31:30 -0500

    paused while ds&a

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 After having learned the DOM manipulation concept with JavaScript, I finally feel ready to tackle this project. 
 
-Prior to ```Calculator```, I was working on getting increasingly familiar with JS syntax via solving algorithmic challenges on CodeWars. I had too much fun, and it's time to shift to a more project-based learning approach. 
+Prior to `Calculator`, I was working on getting increasingly familiar with JS syntax via solving algorithmic challenges on CodeWars. I had too much fun, and it's time to shift to a more project-based learning approach. 
 
-```Calculator``` is 
+
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const calc = document.querySelector("#calculator")
-const number = document.querySelector("#number")
+const numberClick = document.querySelector("#number")
 
+//calculator to be keyboard and click activated
+
+/* finite state machine (FSM) layout:
+n = 6 states
+
+0) default (i.e. blank state / AC)
+1) key(s) [x] pressed & staged (loop operation until state 2))
+2) operator pressed & staged
+3) key(s) [y] pressed & staged (loop operation until state 4)
+4) calc function performed (trigger (=))
+
+*/
+
+const calculation = () => {
+    numberClick +
+}
 
 number.addEventListener('click', () => {
-    calc +=
-})
+    calc += numberClick;
+
+});
