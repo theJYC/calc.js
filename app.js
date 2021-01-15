17260ec75aad9e799b57c6aa90c98bd760aba42e
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-01-15 17:05:11 -0500

    earmark

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const calc = document.querySelector("#calculator")
-const numberClick = document.querySelector("#number")
-
 //calculator to be keyboard and click activated
 
-/* finite state machine (FSM) layout:
+/*
+finite state machine (FSM) layout:
 n = 6 states
 
 0) default (i.e. blank state / AC)
@@ -14,11 +12,49 @@ n = 6 states
 
 */
 
-const calculation = () => {
-    numberClick +
+//basic maths operators
+
+const add = (x,y) => x + y;
+const subtract = (x,y) => x - y;
+const multiply = (x,y) => x * y;
+const divide = (x,y) => x / y;
+
+// operate (takes an operator and 2 numbers)
+
+const operate = (x,y,z) => {
+    switch(z) {
+        case "+":
+            return add(x,y);
+            break;
+        case "-":
+            return subtract(x,y);
+            break;
+        case "*":
+            return multiply(x,y);
+            break;
+        case "/":
+            if (y === 0) {
+                return 'nah';
+            } else {
+                return divide(x,y);
+            }
+            break;
+    }
 }
 
-number.addEventListener('click', () => {
-    calc += numberClick;
+//you want to select all the buttons that are to be manipulated by JS:
 
-});
+const numberKeys = document.querySelectorAll("#number"); //number buttons
+const operatorKeys = document.querySelectorAll("#operator"); //operator button
+const resetKey = document.querySelector("#reset"); //reset button
+const inverseKey = document.querySelector("#inverse"); //inverse button
+const percentKey = document.querySelector("#percent"); //percent button
+const screen = document.querySelector("#screenumber"); // screen value
+
+//functions that populates the display when number button is clicked
+
+const numberClicked = numberKeys.addEventListener("click", addToDisplay);
+
+const addToDisplay = (numberKeys) => {
+    return screen.innerText + numberKeys;
+}
