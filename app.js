a6c691224b67a1b67a9f0fabd7c18078a05d6f4b
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-01-17 07:44:43 -0500

    working, without edge cases

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ const operate = (x,y,z) => {
 
 //select all the buttons that are to be manipulated by JS:
 
-const numberKeys = document.querySelectorAll("#number"); //number buttons
-const operatorKeys = document.querySelectorAll("#operator"); //operator button
+const numberKeys = document.querySelectorAll(".number"); //number buttons
+const operatorKeys = document.querySelectorAll(".operator"); //operator button
 const resetKey = document.querySelector("#reset"); //reset button
 const inverseKey = document.querySelector("#inverse"); //inverse button
 const percentKey = document.querySelector("#percent"); //percent button
@@ -52,10 +52,54 @@ const screen = document.querySelector("#screen-number"); // screen value
 //functions that populate the display when number button is clicked
 
 
-const addToDisplay = () => {
-    return screen.innerText += numberKeys.innerHTML;
+const addToDisplay = (value) => {
+    return screen.innerText += value;
 }
 
-for (number of numberKeys) {
-    number.addEventListener("click", addToDisplay)
+const clearDisplay = () => {
+    return screen.innerText = "";
 }
+// add one event listener to the entire buttons
+let numberOnScreen = false;
+let operatorOnScreen = false;
+let firstNumber; //first number, up until the operator is pressed
+let operator;
+let secondNumber;
+
+const allButtons = document.getElementById("keys");
+
+allButtons.addEventListener("click", (e) => {
+    const target = e.target; //specific button that is clicked
+    if (target.classList.contains("number") && !operator) {
+        addToDisplay(target.innerHTML);
+        numberOnScreen = true;
+        firstNumber = screen.innerHTML;
+    }
+    if (target.classList.contains("operator")) {
+        firstNumber = screen.innerText;
+        operator = target.innerHTML;
+        clearDisplay();
+        addToDisplay(operator);
+        numberOnScreen = false;
+        operatorOnScreen = true;
+    }
+    if (target.classList.contains("number") && operator) {
+        if (operatorOnScreen) {
+            clearDisplay();
+        }
+        addToDisplay(target.innerHTML);
+        numberOnScreen = true;
+        secondNumber = screen.innerHTML;
+    }
+    if (target.classList.contains("equals") && firstNumber && operator && secondNumber) {
+        clearDisplay();
+        firstNumber = parseInt(firstNumber);
+        secondNumber = parseInt(secondNumber);
+        result = operate(firstNumber, secondNumber, operator);
+        addToDisplay(result);
+        firstNumber = result;
+        secondNumber = undefined;
+        operator = undefined;
+    }
+ //nextsteps: allclear (AC) button; float toFixed(); big numbers (to sig figs); negative numbers;
+});
